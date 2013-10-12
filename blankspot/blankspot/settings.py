11b6578ee9c519d37115e706196875146b56e4c8
Frank Lanitz <frank@frank.uvena.de>
2013-10-12 20:11:17 +0200

    Activating admin-interface -- even nothing to see there yet ;)

--- blankspot/blankspot/settings.py
+++ blankspot/blankspot/settings.py
@@ -120,10 +120,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'south',
     'node_registration',
 )
