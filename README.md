0a60ad4f0f5647822de560b1b00da1964662eeb5
Frank Lanitz <frank@frank.uvena.de>
2013-10-12 19:47:58 +0200

    Adding postgres configuration

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Dependencies
 Blankspot depends on:
 - Django and therefor Python
 - A database server supported by django (PostgreSQL recommended)
+
+
+Creating databaseuser
+---------------------
+
+Having default settings on settings.py you will need to run a postgres 
+server on localhost. Needed user and database can be create e.g. with 
+logging in as superuser to database and than:
+
+	CREATE DATABSE blankspot;
+	CREATE ROLE blankspotuser WITH PASSWORD 'hahahfooo' LOGIN;
+	ALTER DATABASE blankspot ONWER TO blankspotuser;
--- blankspot/blankspot/settings.py
+++ blankspot/blankspot/settings.py
@@ -11,13 +11,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'blankspot',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'blankspotuser',
+        'PASSWORD': 'hahahfooo',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
