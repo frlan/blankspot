33ed374f1178d62bf35339a50d1511f076b991fc
Frank Lanitz <frank@frank.uvena.de>
2013-10-12 22:18:38 +0200

    Enable admin interface for node_registration and adding a fist draft of object

---
+++ blankspot/node_registration/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from node_registration.models import position
+
+admin.site.register(position)
--- blankspot/node_registration/models.py
+++ blankspot/node_registration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class position(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    altitude = models.FloatField()
+    latitude = models.FloatField()
+    
+    def __unicode__(self):  
+        return self.email
