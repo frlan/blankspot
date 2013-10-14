4f8dc384e0340f4c563c3a91775c2ec0b8147e04
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-10-14 20:58:32 +0200

    Start to move to class based view as well as adding a about page

--- blankspot/blankspot/urls.py
+++ blankspot/blankspot/urls.py
@@ -1,17 +1,26 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from node_registration import views
+from django.views.generic import TemplateView
+
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'blankspot.views.home', name='home'),
-    # url(r'^blankspot/', include('blankspot.foo.urls')),
-    url(r'^list/', views.list_nodes, name='node_list'),
-    url(r'^$', views.index, name='index'),
-    url(r'^$', include('node_registration.urls')),
-    url(r'^register/', include('node_registration.urls')),
-	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^about/', views.AboutView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+#~ urlpatterns = patterns('',
+    #~ # Examples:
+    #~ # url(r'^$', 'blankspot.views.home', name='home'),
+    #~ # url(r'^blankspot/', include('blankspot.foo.urls')),
+    #~ url(r'^list/', views.list_nodes, name='node_list'),
+    #~ url(r'^about/', TemplateView.as_view(template_name="about.html")),
+    #~ url(r'^$', views.index, name='index'),
+    #~ url(r'^$', include('node_registration.urls')),
+    #~ url(r'^about/', TemplateView.as_view(template_name="about.html")),
+    #~ url(r'^register/', include('node_registration.urls')),
+	#~ url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #~ url(r'^admin/', include(admin.site.urls)),
+#~ )
--- blankspot/node_registration/views.py
+++ blankspot/node_registration/views.py
@@ -2,6 +2,12 @@ from django.http import HttpResponse
 from node_registration.models import position
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+
 
 def index(request):
     node_list = position.objects.all()
