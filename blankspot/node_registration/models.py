8ea36f6a3f0330b8ee6cd466054219559fd456e3
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-10-15 20:26:44 +0200

    Adding a field for nick as well as some adreess desctiption field to model

--- blankspot/node_registration/models.py
+++ blankspot/node_registration/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class position(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254)
-    street = models.CharField(max_length=200, null=False, default="leer")
-    city = models.CharField(max_length=100, null=True, default="Jena")
-    altitude = models.FloatField()
-    latitude = models.FloatField()
-    
-    def __unicode__(self):  
-        return (self.street)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	nick = models.CharField(max_length=128, null=True)
+	email = models.EmailField(max_length=254)
+	street = models.CharField(max_length=200, null=False, default="leer")
+	city = models.CharField(max_length=100, null=True, default="Jena")
+	address_description = models.TextField(null=True)
+	altitude = models.FloatField()
+	latitude = models.FloatField()
+
+	def __unicode__(self):
+		return (self.street)
