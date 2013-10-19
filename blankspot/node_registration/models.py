909c4258518d39156dad1e1d11e717959548d816
Frank Lanitz <frank@frank.uvena.de>
2013-10-19 17:57:34 +0200

    Splitting position and contact

--- blankspot/node_registration/models.py
+++ blankspot/node_registration/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
-class Position(models.Model):
+class Contact(models.Model):
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
 	nick = models.CharField(max_length=128, null=True)
-	email = models.EmailField(max_length=254)
+	email = models.EmailField(max_length=254)	
+	
+	def __unicode__(self):
+		return (self.nick)
+	def get_absolute_url(self):
+		return reverse('contact-detail', kwargs={'pk': self.pk})
+
+class Position(models.Model):
 	street = models.CharField(max_length=200, null=False, default="leer")
 	city = models.CharField(max_length=100, null=True, default="Jena")
 	address_description = models.TextField(null=True)
