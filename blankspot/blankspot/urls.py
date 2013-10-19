b6c3b3462755b339c42856fd4592c994dfcfc18a
Frank Lanitz <frank@frank.uvena.de>
2013-10-19 20:06:26 +0200

    Consolidate urls.py

--- blankspot/blankspot/urls.py
+++ blankspot/blankspot/urls.py
@@ -2,33 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from node_registration import views
-from node_registration.views import PositionCreate, PositionDelete, PositionUpdate, ListPosition
-
-
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^about/', views.AboutView.as_view()),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'position/add/$', PositionCreate.as_view(), name='position_add'),
-    url(r'position/(?P<pk>\d+)/$', PositionUpdate.as_view(), name='position_update'),
-    url(r'position/(?P<pk>\d+)/delete/$', PositionDelete.as_view(), name='position_delete'),
-    url(r'position/list', views.ListPosition.as_view()),
-    #url(r'^register/$', views.RegisterForm.as_view()),
-    #url(r'^registering/$', views.Register.as_view()),
+	url(r'^about/', views.AboutView.as_view()),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'position', include('node_registration.urls')),
 )
 
-#~ urlpatterns = patterns('',
-    #~ # Examples:
-    #~ # url(r'^$', 'blankspot.views.home', name='home'),
-    #~ # url(r'^blan	kspot/', include('blankspot.foo.urls')),
-    #~ url(r'^list/', views.list_nodes, name='node_list'),
-    #~ url(r'^about/', TemplateView.as_view(template_name="about.html")),
-    #~ url(r'^$', views.index, name='index'),
-    #~ url(r'^$', include('node_registration.urls')),
-    #~ url(r'^about/', TemplateView.as_view(template_name="about.html")),
-    #~ url(r'^register/', include('node_registration.urls')),
-	#~ url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    #~ url(r'^admin/', include(admin.site.urls)),
-#~ )
--- blankspot/node_registration/urls.py
+++ blankspot/node_registration/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import patterns, url
 from node_registration import views
 
 urlpatterns = patterns('',
+	url(r'add/$', views.PositionCreate.as_view(), name='position_add'),
+	url(r'/(?P<pk>\d+)/$', views.PositionUpdate.as_view(), name='position_update'),
+	url(r'/(?P<pk>\d+)/delete/$', views.PositionDelete.as_view(), name='position_delete'),
+	url(r'/list', views.ListPosition.as_view()),
 #    url(r'^$', views.list_nodes, name='node_list'),
 #    url(r'^$', views.index, name='index'),
 )
