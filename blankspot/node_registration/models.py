e1e64da8a8e86bb74c5edecf6911d9d8f608735f
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-10-21 21:52:28 +0200

    Adjusting some more blank and null options for fields as well as updating default values

--- blankspot/node_registration/models.py
+++ blankspot/node_registration/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Contact(models.Model):
-	first_name = models.CharField(max_length=50, blank=True)
-	last_name = models.CharField(max_length=50, blank=True)
+	first_name = models.CharField(max_length=50, blank=True, null=True)
+	last_name = models.CharField(max_length=50, blank=True, null=True)
 	nick = models.CharField(max_length=128)
-	email = models.EmailField(max_length=254)	
-	
+	email = models.EmailField(max_length=254)
+
 	def __unicode__(self):
 		return (self.nick)
 
@@ -14,11 +14,11 @@ class Contact(models.Model):
 
 class Position(models.Model):
 	contact = models.ForeignKey('Contact')
-	street = models.CharField(max_length=200, blank=True, default="leer")
-	city = models.CharField(max_length=100, blank=True, default="Jena")
-	address_description = models.TextField(blank=True)
+	street = models.CharField(max_length=200, blank=True, null=True)
+	city = models.CharField(max_length=100, blank=True, null=True)
+	address_description = models.TextField(blank=True, null=True)
 	altitude = models.FloatField(blank=True, null=True)
-	latitude = models.FloatField(blank=True, null= True)
+	latitude = models.FloatField(blank=True, null=True)
 
 	def __unicode__(self):
 		return (self.street)
