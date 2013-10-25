06ec0a7f0a6a53fddfb2038b0ae8cc1bad2c8511
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-10-25 10:49:20 +0200

    Revert splitting of model as its adding to much complexitiy for the timebeing to later logics IIt's just not adding enought value for having a more complicated implementation.

--- blankspot/node_registration/models.py
+++ blankspot/node_registration/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 
-class Contact(models.Model):
+class Position(models.Model):
 	first_name = models.CharField(max_length=50, blank=True, null=True)
 	last_name = models.CharField(max_length=50, blank=True, null=True)
 	nick = models.CharField(max_length=128)
 	email = models.EmailField(max_length=254)
-
-	def __unicode__(self):
-		return (self.nick)
-
-	def get_absolute_url(self):
-		return reverse('contact-detail', kwargs={'pk': self.pk})
-
-class Position(models.Model):
-	contact = models.ForeignKey('Contact')
 	street = models.CharField(max_length=200, blank=True, null=True)
 	city = models.CharField(max_length=100, blank=True, null=True)
 	address_description = models.TextField(blank=True, null=True)
