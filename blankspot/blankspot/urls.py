c84dedded5ef24e5a3def8d71364ae59801e2b77
Frank Lanitz <frank@frank.uvena.de>
2013-11-02 18:07:43 +0100

    Adding a todo-Liste accesable via url ... Not sure whetehr it's the best idea I had today.

--- blankspot/blankspot/urls.py
+++ blankspot/blankspot/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
+	url(r'^todo$', TemplateView.as_view(template_name="todo.html"), name='todo'),
 	url(r'^about/', TemplateView.as_view(template_name="about.html"), name='about'),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'position', include('node_registration.urls')),
