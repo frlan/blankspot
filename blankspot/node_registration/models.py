e294dd3747fb795fbc7a647ceebe8ddc4b7631c7
Frank Lanitz <frank@frank.uvena.de>
2013-11-02 18:11:27 +0100

    Adding some human readable labels for model

--- blankspot/node_registration/models.py
+++ blankspot/node_registration/models.py
@@ -2,13 +2,13 @@ from django.core.urlresolvers import reverse
 from django.db import models
 
 class Position(models.Model):
-	first_name = models.CharField(max_length=50, blank=True, null=True)
-	last_name = models.CharField(max_length=50, blank=True, null=True)
-	nick = models.CharField(max_length=128)
+	first_name = models.CharField(verbose_name="Vorname", max_length=50, blank=True, null=True)
+	last_name = models.CharField(verbose_name="Nachname", max_length=50, blank=True, null=True)
+	nick = models.CharField(verbose_name="Spitzname", max_length=128)
 	email = models.EmailField(max_length=254)
-	street = models.CharField(max_length=200, blank=True, null=True)
-	city = models.CharField(max_length=100, blank=True, null=True)
-	address_description = models.TextField(blank=True, null=True)
+	street = models.CharField(verbose_name="Strasse", max_length=200, blank=True, null=True)
+	city = models.CharField(verbose_name="Stadt", max_length=100, blank=True, null=True)
+	address_description = models.TextField("Genauere Beschreibung der Adresse. Z.B. Dachwohnung mit Blick auf den Turm", blank=True, null=True)
 	longitude = models.FloatField(blank=True, null=True)
 	latitude = models.FloatField(blank=True, null=True)
 
