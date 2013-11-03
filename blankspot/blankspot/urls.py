03cae99b72ee9574477ee7e308e60edbd566fee6
Frank Lanitz <frank@frank.uvena.de>
2013-11-03 14:48:16 +0100

    Fixing / inside URL

--- blankspot/blankspot/urls.py
+++ blankspot/blankspot/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
 	url(r'^todo$', TemplateView.as_view(template_name="todo.html"), name='todo'),
 	url(r'^about/', TemplateView.as_view(template_name="about.html"), name='about'),
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'position', include('node_registration.urls')),
+	url(r'position/', include('node_registration.urls')),
 )
 
--- blankspot/node_registration/urls.py
+++ blankspot/node_registration/urls.py
@@ -4,7 +4,7 @@ from node_registration import views
 
 urlpatterns = patterns('',
 	url(r'add/$', views.PositionCreate.as_view(), name='position-add'),
-	url(r'/(?P<pk>\d+)/$', views.PositionUpdate.as_view(), name='position-update'),
-	url(r'/(?P<pk>\d+)/delete/$', views.PositionDelete.as_view(), name='position-delete'),
-	url(r'/list', views.ListPosition.as_view(), name='position-list'),
+	url(r'(?P<pk>\d+)/$', views.PositionUpdate.as_view(), name='position-update'),
+	url(r'(?P<pk>\d+)/delete/$', views.PositionDelete.as_view(), name='position-delete'),
+	url(r'list', views.ListPosition.as_view(), name='position-list'),
 )
