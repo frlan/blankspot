9ced2bb25fe669e70aa74386c9e4f783aecec887
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-11-07 17:24:08 +0100

    Updating verase name with start to show its required and a varbose name for model

--- blankspot/node_registration/models.py
+++ blankspot/node_registration/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Position(models.Model):
 	first_name = models.CharField(verbose_name="Vorname", max_length=50, blank=True, null=True)
 	last_name = models.CharField(verbose_name="Nachname", max_length=50, blank=True, null=True)
-	nick = models.CharField(verbose_name="Spitzname", max_length=128)
-	email = models.EmailField(max_length=254)
+	nick = models.CharField(verbose_name="Spitzname*", max_length=128)
+	email = models.EmailField(verbose_name="Email*", max_length=254)
 	street = models.CharField(verbose_name="Strasse", max_length=200, blank=True, null=True)
 	city = models.CharField(verbose_name="Stadt", max_length=100, blank=True, null=True)
 	address_description = models.TextField("Genauere Beschreibung der Adresse. z.B. Dachwohnung mit Blick auf den Turm", blank=True, null=True)
@@ -27,3 +27,7 @@ class Position(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('position-list')
+
+	class Meta:
+		verbose_name = (u'Position')
+		verbose_name_plural = (u'Positions')
