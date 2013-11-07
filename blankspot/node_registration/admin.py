5e8aa218eee6d16ebd92fdc5bf7f09f55e621063
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-11-07 16:58:32 +0100

    Show timestamp also on list on admin interface for position

--- blankspot/node_registration/admin.py
+++ blankspot/node_registration/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from node_registration.models import Position
 
 class PositionAdmin(admin.ModelAdmin):
-	list_display = ['nick', 'street', 'longitude', 'latitude']
+	list_display = ['nick', 'street', 'longitude', 'latitude', 'timestamp']
 	search_fields = ['nick', 'street']
 
 admin.site.register(Position, PositionAdmin)
