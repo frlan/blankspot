bab77e6653cf2da4f70afc7fe8270457ad483671
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-11-10 16:18:03 +0100

    Anoter fix of urls

--- blankspot/node_registration/urls.py
+++ blankspot/node_registration/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from node_registration import views
 
 urlpatterns = patterns('',
-	url(r'add/$', views.PositionCreate.as_view(), name='position-add'),
-	url(r'(?P<pk>\d+)/$', views.PositionUpdate.as_view(), name='position-update'),
-	url(r'(?P<pk>\d+)/delete/$', views.PositionDelete.as_view(), name='position-delete'),
-	url(r'list', views.ListPosition.as_view(), name='position-list')
+	url(r'^add/$', views.PositionCreate.as_view(), name='position-add'),
+	url(r'^(?P<pk>\d+)/$', views.PositionUpdate.as_view(), name='position-update'),
+	url(r'^(?P<pk>\d+)/delete/$', views.PositionDelete.as_view(), name='position-delete'),
+	url(r'^list', views.ListPosition.as_view(), name='position-list')
 )
