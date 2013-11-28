4f477097fff4e506d29168dbb19e9cf08fdd9232
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-11-28 11:53:06 +0100

    Fixing merge failure

--- blankspot/node_registration/views.py
+++ blankspot/node_registration/views.py
@@ -10,15 +10,6 @@ from django.core.mail import EmailMessage
 from django.views.generic.base import View
 from django.utils.translation import ugettext as _
 
-class ContactView(FormView):
-    template_name = 'contact.html'
-    success_url = '/thanks/'
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        form.send_email()
-        return super(ContactView, self).form_valid(form)
 
 class PositionCreate(CreateView):
     model = Position
