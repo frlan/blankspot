91d63e50df5bc8b9fe7d98b28efd541bafd0bc08
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-11-28 10:20:28 +0100

    Replace \t by spaces for indention

--- blankspot/node_registration/urls.py
+++ blankspot/node_registration/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from node_registration import views
 
 urlpatterns = patterns('',
-	url(r'^add/$', views.PositionCreate.as_view(), name='position-add'),
-	url(r'^list', views.ListPosition.as_view(), name='position-list')
+    url(r'^add/$', views.PositionCreate.as_view(), name='position-add'),
+    url(r'^list', views.ListPosition.as_view(), name='position-list')
 )
