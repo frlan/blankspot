46f04a02fce01306ed863f38346974e3bf587681
Frank Lanitz <flanitz@bgc-jena.mpg.de>
2013-12-20 21:47:08 +0100

    Removing ToDo list from local template set and link to Github page

--- blankspot/blankspot/urls.py
+++ blankspot/blankspot/urls.py
@@ -9,7 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
 	url(r'^contact/$', ContactView.as_view(), name='contact'),
-	url(r'^todo$', TemplateView.as_view(template_name="todo.html"), name='todo'),
 	url(r'^about/', TemplateView.as_view(template_name="about.html"), name='about'),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^position/', include('node_registration.urls')),
